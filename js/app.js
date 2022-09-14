740285cef4dc837d22d2fa5bbc3baa8801223a70
Mohamed H Noor <mohmed.hassannoor114@gmail.com>
2022-09-15 01:36:55 +0200

    refactored the mobile feature according to design guidline

--- js/app.js
+++ js/app.js
@@ -1,13 +1,14 @@
 // mobile menu
 const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelectorAll('.nav-link');
+
 navToggle.addEventListener('click', () => {
-  document.body.classList.toggle('nav-open');
+  document.body.classList.toggle('show-links');
 });
 
 navLinks.forEach((link) => {
   link.addEventListener('click', () => {
-    document.body.classList.remove('nav-open');
+    document.body.classList.remove('show-links');
   });
 });
 
